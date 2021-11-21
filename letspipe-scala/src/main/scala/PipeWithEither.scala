5fdd23f193b29b51df3637c50b7f51b2751c5601
Thomas Effing <thomas.effing@sbb.ch>
2021-11-21 23:49:25 +0100

    correct PipeWithEither.scala

--- letspipe-scala/src/main/scala/PipeWithEither.scala
+++ letspipe-scala/src/main/scala/PipeWithEither.scala
@@ -15,34 +15,37 @@ class PipeWithEither:
       .flatMap(host => ping(host))
     println(s"pinging successful: $success")
 
-  def ping(url: String): Option[Boolean] =
-    val host = InetAddress.getByName(url)
-    if (host.isReachable(5000)) Right(true) else Left(s"unable to reach $url")
+  def ping(url: String): Either[String, Boolean] =
+    try
+      val host = InetAddress.getByName(url)
+      if (host.isReachable(5000)) Right(true) else Left(s"unable to reach $url")
+    catch case e: Exception => Left(e.toString)
 
   def valueOfProp(propLine: String): Either[String, String] =
     val lines = propLine.split("=")
     if (lines.length == 2) Right(lines(1)) else Left(s"no value in propLine $propLine")
 
   def grepLineWithPrefix(searchString: String, content: Source): Either[String, String] =
-    val regex: Regex = searchString.concat(".*").r
+    val regex: Regex = "^".concat(searchString).concat(".*").r
     content
       .getLines()
       .filter(line => regex.findFirstIn(line).isDefined)
       .nextOption match 
         case Some(x) => Right(x)
-        case None => s"regex $regex not found in $content"
+        case None => Left(s"regex $regex not found in $content")
 
   def catResourcesFile(fileName: String): Either[String, String] =
     try
       val resourcesDirectory = File("src/main/resources")
       val path = Paths.get(resourcesDirectory.getAbsolutePath() + "/" + fileName)
       Right(Files.readString(path, StandardCharsets.UTF_8))
-    catch case e: Exception => Lift(e)
+    catch case e: Exception => Left(e.toString)
 
-    def runPipeElegant() =
-      val success = for 
-        lines <- catResourcesFile("config.properties")
-        hostLine <- grepLineWithPrefix("host", Source.fromString(lines))
-        host <- valueOfProp(hostLine)
-      yield ping(host)
+  def runPipeElegant(): Unit =
+    val success = for 
+      lines     <- catResourcesFile("config.properties")
+      hostLine  <- grepLineWithPrefix("host", Source.fromString(lines))
+      host      <- valueOfProp(hostLine)
+      sucess    <- ping(host)
+    yield sucess
     println(s"pinging successful: $success")
