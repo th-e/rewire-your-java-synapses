590c13b9ae640fdc0693cfdc1c1460edecc1b725
Thomas Effing <thomas.effing@sbb.ch>
2021-11-22 23:26:35 +0100

    formatting

--- hkt-scala/src/main/scala/de/webtuples/TypeLevels.scala
+++ hkt-scala/src/main/scala/de/webtuples/TypeLevels.scala
@@ -2,19 +2,18 @@ package de.webtuples
 
 import cats.effect.IO
 import cats.*
-import cats.Monad.ops.toAllMonadOps   //uff
+import cats.Monad.ops.toAllMonadOps //uff
 
 object TypeLevels:
 
-    def firstLevel(a: Int, b: Int) =
-        a + b
+  def firstLevel(a: Int, b: Int) =
+    a + b
 
-    def secondLevel[T](a: List[T], b: List[T]) =
-        a ++ b
+  def secondLevel[T](a: List[T], b: List[T]) =
+    a ++ b
 
-    def thirdLevel[F[_]: Monad](a: F[Int], b: F[Int]): F[Int] = for 
-        x <- a
-        y <- b
+  def thirdLevel[F[_]: Monad](a: F[Int], b: F[Int]): F[Int] = 
+    for
+      x <- a
+      y <- b
     yield x + y
-
-  
